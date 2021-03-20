09fd6f3eeae2e5788a416f564c1073b37702bfd3
Lucas Vieira <lucas@devplace.com.br>
2021-03-19 22:17:10 -0300

    publishing

--- build/src/index.js
+++ build/src/index.js
@@ -1,6 +1,20 @@
 "use strict";
-console.log('Try npm run lint/fix!!');
-console.log('Try npm run lint/fix!!');
-debugger;
-console.log('!!!');
+Object.defineProperty(exports, "__esModule", { value: true });
+const amqp = require("amqplib");
+const url = 'amqps://fynnrolf:pF11ivy4p58KK9SgzQSblhW_v1fWxIDx@chimpanzee.rmq.cloudamqp.com/fynnrolf';
+const q = 'hello';
+async function publish() {
+    try {
+        const conn = await amqp.connect(url);
+        const ch = await conn.createChannel();
+        ch.assertQueue(q, { durable: false });
+        const msg = `${Date.now()}`;
+        ch.sendToQueue(q, Buffer.from(msg));
+        console.log(`Message sent:\n${msg}\n`);
+    }
+    catch (error) {
+        console.warn(error);
+    }
+}
+setInterval(publish, 5e3);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Try npm run lint/fix!!');
+import * as dotenv from 'dotenv';
+import * as amqp from 'amqplib';
+
+dotenv.config();
+
+const {QUEUE_URL} = process.env;
+const q = 'hello';
+
+async function publish() {
+  try {
+    const conn = await amqp.connect(QUEUE_URL);
+    const ch = await conn.createChannel();
+    ch.assertQueue(q, {durable: false});
+    const msg = `${Date.now()}`;
+    ch.sendToQueue(q, Buffer.from(msg));
+    console.log(`Message sent:\n${msg}\n`);
+  } catch (error) {
+    console.warn(error);
+  }
+}
+setInterval(publish, 5e3);
