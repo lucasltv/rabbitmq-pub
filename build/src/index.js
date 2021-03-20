c08677caef381ea5d526194e5ff1c4c0d57dabfa
Lucas Vieira <lucas@devplace.com.br>
2021-03-19 21:39:08 -0300

    ts

--- build/src/index.js
+++ build/src/index.js
@@ -1,20 +1,6 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.doSomeStuff = void 0;
-console.log('Try npm run lint/fix!');
-const longString = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
-const trailing = 'Semicolon';
-const why = 'am I tabbed?';
-function doSomeStuff(withThis, andThat, andThose) {
-    //function on one line
-    if (!andThose.length) {
-        return false;
-    }
-    console.log(withThis);
-    console.log(andThat);
-    console.dir(andThose);
-    return;
-}
-exports.doSomeStuff = doSomeStuff;
-// TODO: more examples
+console.log('Try npm run lint/fix!!');
+console.log('Try npm run lint/fix!!');
+debugger;
+console.log('!!!');
 //# sourceMappingURL=index.js.map
